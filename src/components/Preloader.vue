0326dac6bf79aa2dc394a7c0a92bd8591c202f58
EdsonJSF <Edsonjsf1990@gmail.com>
2021-07-06 16:29:29 -0400

    preloader con posicion fija

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,10 +1,9 @@
 <template>
-    <div
-        v-if="preloader"
-        class="Preloader position-absolute d-flex justify-content-center align-items-center"
-    >
-        <div class="Preloader__content">
-            <div class="preloader__animation"></div>
+    <div v-if="preloader" class="Preloader position-absolute">
+        <div
+            class="Preloader__content d-flex justify-content-center align-items-center w-100 h-100"
+        >
+            <div class="Preloader__content-animation"></div>
         </div>
     </div>
 </template>
@@ -25,16 +24,21 @@ export default {
     z-index: 1000;
     min-width: 100vw;
     min-height: 100vh;
+
+    .Preloader__content {
+        position: fixed;
+
+        .Preloader__content-animation {
+            width: 4rem;
+            height: 4rem;
+            border: 0.7rem solid var(--bs-secondary);
+            border-right-color: var(--bs-primary);
+            border-radius: 100%;
+            animation: girar 0.7s linear infinite;
+        }
+    }
 }
-.preloader__animation {
-    width: 4rem;
-    height: 4rem;
-    border: 0.7rem solid var(--bs-secondary);
-    border-right-color: var(--bs-primary);
-    border-radius: 100%;
-    animation: preloader 0.7s linear infinite;
-}
-@keyframes preloader {
+@keyframes girar {
     0% {
         transform: rotate(0deg);
     }
